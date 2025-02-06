2311d529947805e945f62b11c04dbb53f4829c30
jamesnazzelofficial <148680292+jamesnazzelofficial@users.noreply.github.com>
2025-02-06 22:40:36 +0800

    Update ready.js

--- ready.js
+++ ready.js
@@ -4,6 +4,6 @@ export const logBotInfo = () => {
     console.log("Bot Version: 1.0.0"); // Palitan ayon sa iyong bersyon
     console.log("Developed By: Nazzel Official");
     console.log("All Rights Reserved:");
-    console.log("Fate and Vibes 2024");
+    console.log("Fate and Vibes 2025");
     console.log("__________________");
 };
